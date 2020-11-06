d016d214872acafbc102fc23c3c6236bc0de50fc
ilaydabykakova <ilaydabuyukakova27@gmail.com>
2020-11-07 00:13:52 +0300

    Initial commit

---
+++ Instagram.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Instagram.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Profil2()
+        {
+            return View();
+        }
+    }
+}
+}
---
+++ Instagram.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Instagram.WebUI.MvcApplication" Language="C#" %>
